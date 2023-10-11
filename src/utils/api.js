696caaf33eabacaa1ed1dfb0b03733df2f9bf7cb
Aasim Taif <taifullahansari@gmail.com>
2023-10-11 18:47:42 +0530

    debugging

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const API = axios.create({
     baseURL:
-        "http://localhost:2000/"
-        // "https://resume-generator-backend.onrender.com/"
+        // "http://localhost:2000/"
+        "https://resume-generator-backend.onrender.com/"
     ,
 });
 
